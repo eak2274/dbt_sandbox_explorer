aeeadb02c8477e05b521718b1be2ff8245a24097
Yevgeni Kosterin <sparepart74@gmail.com>
2025-02-25 21:06:49 +0000

    All queries have been adjusted to account for the specifics of BigQuery.

--- models/metrics/metricflow_time_spine.sql
+++ models/metrics/metricflow_time_spine.sql
@@ -1,13 +1,11 @@
 -- metricflow_time_spine.sql
 --for BQ adapters use "DATE('01/01/2000','mm/dd/yyyy')"
 with days as (
-    {{dbt_utils.date_spine('day'
-    , "to_date('01/01/2000','mm/dd/yyyy')"
-    , "to_date('01/01/2027','mm/dd/yyyy')"
-    )
-    }}
+    {{ dbt_utils.date_spine('day'
+        , "DATE('2000-01-01')"
+        , "DATE('2027-01-01')"
+    ) }}
 ),
-
 final as (
     select cast(date_day as date) as date_day
     from days
